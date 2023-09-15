5e614066ddd009bf0cf22e49be63491454acbfb8
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-09-14 21:43:36 -0500

    chore: Add mod utils

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+mod utils;
+
+use colored::*;
+use std::env;
+use utils::models;
+
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("\n");
+    let welcome_txt = String::from("Hello, Welcome to Miu 🐱").green().bold();
+
+    let args: Vec<String> = env::args().collect();
+
+
+    models::CLI::Cat(String::from("Value"));
+    
+    println!("{:#?}", args);
+    println!("{}", welcome_txt);
 }
