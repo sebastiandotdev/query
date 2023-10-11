71a12f88fe9b3b06368f00f6a3cdc4ec3ecff776
castrogarciajs <johancs.mm@gmail.com>
2023-10-11 10:24:55 -0500

    feat(🚀): Add tokio

--- bin/main.rs
+++ bin/main.rs
@@ -1,8 +1,17 @@
-fn main() {
-  println!("Hello, world!");
-  run();
+use reqwest::Client;
+
+
+#[tokio::main]
+async fn main() {
+    println!("Hello, world!");
+    run().await;
 }
 
-fn run() {
-  println!("Hello, world!");
+
+async fn run() {
+
+  let client = Client::new();
+  let result = client.get("https://jsonplaceholder.typicode.com/users").send().await;
+
+  println!("{:#?}", result);
 }
