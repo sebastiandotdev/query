162b18ddaf3157ec1c8e2334374db34857594a4c
castrogarciajs <johancs.mm@gmail.com>
2023-10-13 20:28:14 -0500

    feat(bin): create file minify.json

--- bin/args.rs
+++ bin/args.rs
@@ -1,10 +1,23 @@
 use reqwest::Client;
+use serde::Deserialize;
+use std::fs::File;
 
 use crate::methods;
 
+#[derive(Deserialize, Debug)]
+struct MinifyConfig {
+  url_base: String,
+}
+
 pub async fn run(method: &String, url: &String) {
+  let file = File::open("minify.config.json").expect("Unable to open file");
+
+  let config: MinifyConfig =
+    serde_json::from_reader(file).expect("Unable to read file");
+
   let client = Client::new();
-  let base_url = "https://jsonplaceholder.typicode.com";
+  let base_url = &config.url_base;
+
   let url = format!("{}{}", base_url, url);
 
   actions(method, client, url).await;
