b6bf2f55b00217bfd1206dea733d11b5da138938
castrogarciajs <johancs.mm@gmail.com>
2023-10-16 10:52:39 -0500

    feat(bin): restarting features

--- bin/main.rs
+++ bin/main.rs
@@ -1,35 +1,3 @@
-/**
- * COPYRIGTH CLIPPY INICIALIZATION CLI
- * Minify -- Software Open Sources
- * maintained by @castrogarciajs
- *  github.com/castrogarciajs
-*/
-mod args;
-mod config;
-mod methods;
-
-use clap::Parser;
-use config::CreateConfig;
-
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-  #[arg(short, long, default_value = "")]
-  method: String,
-
-  #[arg(short, long, default_value = "")]
-  url: String,
-
-  #[arg(short, long, default_value = "minify.json")]
-  config: String,
-}
-
-#[tokio::main]
-async fn main() {
-  let args = Args::parse();
-
-  let minify_config = CreateConfig::new();
-
-  minify_config.config(args.config);
-  args::run(&args.method, &args.url).await;
+fn main() {
+    println!("Hello, world!");
 }
