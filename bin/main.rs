dfde739324159afc43c013f6692bd61a2908bdbb
castrogarciajs <johancs.mm@gmail.com>
2023-10-16 15:01:45 -0500

    feat(bin): add new flag command

--- bin/main.rs
+++ bin/main.rs
@@ -1,3 +1,38 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+  #[arg(short, long)]
+  config: String,
+}
+
 fn main() {
-  println!("Hello, world!");
+  let args = Args::parse();
+
+  let init_config = CommandConfig::new();
+
+  match args.config.as_str() {
+    "init" => init_config.create(&args.config),
+    _ => println!("No command found"),
+  }
+}
+
+pub struct CommandConfig {
+  init: String,
+}
+
+impl CommandConfig {
+  fn new() -> CommandConfig {
+    const COMMAND_CONFIG: &str = "init";
+    CommandConfig {
+      init: String::from(COMMAND_CONFIG),
+    }
+  }
+
+  fn create(&self, option: &String) {
+    if option == &self.init {
+      println!("Init command");
+    }
+  }
 }
