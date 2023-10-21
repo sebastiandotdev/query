c474bebfd11e6402b2b71f2a2631fe4f6c31b6a5
Johan Sebastián <johancs.mm@gmail.com>
2023-10-21 14:02:38 -0500

    docs(playground): update info

--- playground/main.js
+++ playground/main.js
@@ -1,11 +1,8 @@
 /**
  * Playground for fetchy with Deno
- * @packageDocumentation
- * @module playground
- * @ignore
  * Github: https://github.com/castrogarciajs/fetchy
  * 2023 - Sebastian Castro García - MIT License
- * @version 0.0.1
+ * @version 0.1.0
  */
 
 Deno.serve(() => new Response('Hello World'))
