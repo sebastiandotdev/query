d54ea7a00fdf1b75cbd90d0b304d6c3e94d4ada0
castrogarciajs <johancs.mm@gmail.com>
2023-10-21 14:00:07 -0500

    feat: add in the playground serve with deno

---
+++ playground/main.js
@@ -0,0 +1,11 @@
+/**
+ * Playground for fetchy with Deno
+ * @packageDocumentation
+ * @module playground
+ * @ignore
+ * Github: https://github.com/castrogarciajs/fetchy
+ * 2023 - Sebastian Castro García - MIT License
+ * @version 0.0.1
+ */
+
+Deno.serve(() => new Response('Hello World'))
