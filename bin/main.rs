61dbcfa14a1685f4272ef2960a4d6ca675817702
castrogarciajs <johancs.mm@gmail.com>
2023-10-21 21:10:32 -0500

    feat(bin): add func for read method

--- bin/main.rs
+++ bin/main.rs
@@ -5,18 +5,23 @@
  * https://rustlanges.github.io/rust-book-es/
  */
 mod error;
+mod read;
 
 use clap::Parser;
 use colored::Colorize;
 use error::CustomError;
+use read::ReadConfigFetchy;
 use serde_json::{json, to_string_pretty};
 use std::{fs, process};
 
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-  #[arg(short, long)]
+  #[arg(short, long, default_value = "fetchy.json")]
   config: String,
+
+  #[arg(short, long)]
+  method: String,
 }
 pub struct CommandConfig {
   init: String,
@@ -63,13 +68,58 @@ impl CommandConfig {
   }
 }
 
+struct Method {
+  get: String,
+  post: String,
+  put: String,
+  delete: String,
+}
+
+impl Method {
+  fn new(args: Method) -> Self {
+    Self {
+      get: args.get,
+      post: args.post,
+      put: args.put,
+      delete: args.delete,
+    }
+  }
+   fn method_get(&self, option: &String) -> Result<(), CustomError> {
+
+    if &self.get != option {
+      let err = CustomError::new("Invalid command see the --help option");
+      return Err(err);
+    }
+
+    let read_config = ReadConfigFetchy::new();
+    
+   
+
+    Ok(())
+  }
+}
+
 fn main() {
   let args = Args::parse();
   const COMMAND_CONFIG: &str = "init";
+  let methods = Method {
+    get: String::from("get"),
+    post: String::from("post"),
+    put: String::from("put"),
+    delete: String::from("delete"),
+  };
+
   let init_config = CommandConfig::new(COMMAND_CONFIG);
+  let methods_config = Method::new(methods);
 
-  let create_config: Result<(), CustomError> = init_config.create(&args.config);
+  let create_config = init_config.create(&args.config);
 
+  let get = methods_config.method_get(&args.method);
+
+  if let Err(err) = get {
+    eprintln!("{}", err);
+    process::exit(1);
+  }
   if let Err(err) = create_config {
     eprintln!("{}", err);
     process::exit(1);
