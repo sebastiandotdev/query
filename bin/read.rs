7250890c767d2be96823eb4057dcc3451abcd2d1
castrogarciajs <johancs.mm@gmail.com>
2023-10-21 21:10:09 -0500

    feat(bin): add func for read file

---
+++ bin/read.rs
@@ -0,0 +1,20 @@
+use crate::error::CustomError;
+use std::fs;
+
+pub struct ReadConfigFetchy {
+  pub base_url: String,
+}
+
+impl ReadConfigFetchy {
+  pub fn new() -> Result<ReadConfigFetchy, CustomError> {
+    let json = fs::read_to_string("fetchy.json");
+    if json.is_err() {
+      let err = CustomError::new("Error reading file");
+      return Err(err);
+    }
+    let json = json.unwrap();
+    Ok(ReadConfigFetchy {
+      base_url: String::from(json),
+    })
+  }
+}
