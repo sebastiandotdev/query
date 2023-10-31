e9c92273935cb623daa07a603a8c40b860a594a0
castrogarciajs <johancs.mm@gmail.com>
2023-10-31 15:28:27 -0500

    feat: add data example

--- playground/main.js
+++
@@ -1,8 +0,0 @@
-/**
- * Playground for fetchy with Deno
- * Github: https://github.com/castrogarciajs/fetchy
- * 2023 - Sebastian Castro García - MIT License
- * @version 0.1.0
- */
-
-Deno.serve(() => new Response('Hello World'))
