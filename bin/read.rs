e807414018d4f4832ef0f320300085b31c92b88c
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-11-05 22:27:03 -0500

    feat: rename tool cli

--- bin/read.rs
+++ bin/read.rs
@@ -2,18 +2,18 @@ use crate::error::CustomError;
 use std::fs;
 
 #[derive(Debug)]
-pub struct ReadConfigFetchy {
+pub struct ReadConfigQuery {
   pub json: String,
 }
 
-impl ReadConfigFetchy {
-  pub fn new() -> Result<ReadConfigFetchy, CustomError> {
-    let json = fs::read_to_string("fetchy.json");
+impl ReadConfigQuery {
+  pub fn new() -> Result<ReadConfigQuery, CustomError> {
+    let json: Result<String, std::io::Error> = fs::read_to_string("query.json");
     if json.is_err() {
       let err = CustomError::new("Error reading file");
       return Err(err);
     }
     let json = json.unwrap();
-    Ok(ReadConfigFetchy { json })
+    Ok(ReadConfigQuery { json })
   }
 }
